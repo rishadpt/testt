4f54ffd10ed44565f539545aba442725ea6ddf33
rishadpt <rishadpt16@gmail.com>
2023-03-06 16:46:21 +0530

    aws export force added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('/sw.js')
       .then(registration => {
         console.log('Service worker registered:', registration);
       })
